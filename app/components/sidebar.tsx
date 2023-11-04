6fd51c131cfd2a41c89cee502e31228363f98df7
wqj <wqjuser@gmail.com>
2023-11-04 21:34:59 +0800

    修改仓库指向地址，避免覆盖本地文件

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,11 +145,9 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          知心AI助手
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-sub-title"]}>欢迎使用知心AI助手</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
